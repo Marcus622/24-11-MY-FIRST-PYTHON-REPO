a74b0aae3b870373dbf700f8501d1673838de70b
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-12 23:09:03 +0100

    Create Übungen.py

---
+++ Übungen.py
@@ -0,0 +1,12 @@
+# Übungen nach Anleitung https://www.youtube.com/watch?v=e6vPt_e9sRw
+
+print("1. Variable")
+x = 5
+print(x)
+# Wird x in Klammern ohne "" geschrieben, wird der Wert für x, also 5 ausgegeben, mit "" wird x ausgegeben.
+# Die Definition muss vor dem print stehen.
+
+# Übung Email mit Rirmenname als Variable
+
+firmenname = "Techstarter"
+print("Hallo Liebes Team von " + firmenname + ", ich möchte gerne mit euch in der Firma " + firmenname + " arbeiten. Ich würde mich über eine Antwort freuen.") 
