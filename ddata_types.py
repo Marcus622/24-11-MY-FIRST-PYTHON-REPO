d0f580230880833a71a6820edd0b12bcf5b2145e
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-12 11:24:08 +0100

    Create ddata_types.py

---
+++ ddata_types.py
@@ -0,0 +1,14 @@
+# zahlen
+age = 21
+points = 21,5
+
+print(f"Der Typ von age ist{type(age)}")
+# int 64 bit reserviert 2^64
+print(f"Der Typ von points ist{type(points)}")
+# float auch 64 bit
+
+# string
+name = "Max Mustermann"
+# Boolean
+light_is_on = True #False
+is_adult = False
