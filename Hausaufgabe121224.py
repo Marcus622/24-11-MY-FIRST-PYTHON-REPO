e99e111f6244cd960d1c1a90f5204166730cbb31
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-12 16:18:59 +0100

    Create Hausaufgabe121224.py

---
+++ Hausaufgabe121224.py
@@ -0,0 +1,14 @@
+
+# Aufgabe 1
+
+Vorname = input("Wie ist dein Vorname? ")
+Nachname= input("Wie ist dein Nachname? ")
+
+print(f"Dein Name lautet: {Vorname} {Nachname}")
+
+# Aufgabe 2
+
+zahl1 = int(input("Gib die erste Zahl ein: "))
+zahl2 = int(input("Gib die zweite Zahl ein: "))
+
+print(f"Das Ergebnis beider Zahlen lautet: {zahl1+zahl2}")
