d3121cb004b1e78e9ce93eb72253c4ead1c8bfbb
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-13 19:24:07 +0100

    Update Hausaufgabe121224.py

--- Hausaufgabe121224.py
+++ Hausaufgabe121224.py
@@ -20,9 +20,9 @@ print(f"Das Ergebnis beider Zahlen lautet: {zahl1+zahl2}")
 zahl3 = int(input("Gib eine beliebige Zahl ein: "))
 
 
-if zahl3 >=0:
-    print("Die angegebene Zahl ist positiv")
+if zahl3 >=0: # True --> Jump to the next line
+    print("Die angegebene Zahl ist positiv") 
 
 else:     
     print("Die angegebene Zahl ist negativ")
-    
+
