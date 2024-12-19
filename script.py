daf3ff04e925d08ce2c5095ebaf2295cdcf4efef
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-19 14:37:47 +0100

    Delete script.py

--- script.py
+++
@@ -1,9 +0,0 @@
-first_name = "Marcus"
-last_name = "V."
-print (first_name+" "+last_name)
-
-zahl1 = 1
-zahl2 = 10
-print("Zahl 1 + Zahl 2:", zahl1 + zahl2)
-print("Zahl 1 * Zahl 2:", zahl1*zahl2)
-print("Zahl 1 / Zahl 2:", zahl1 / zahl2)           
