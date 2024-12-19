0deae56fe7350f6e85cf069de7db9ed1c4260cf0
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-19 14:37:50 +0100

    Create True_False.py.py

---
+++ True_False.py.py
@@ -0,0 +1,23 @@
+# Aufgabe 1
+
+print("Aufgabe 1 Vergleichsoperationen")
+a = 17
+b = 25
+print("a > b", (a > b))
+print("a < b", (a < b))
+print("a == b", (a == b))
+print("a != b", (a != b))
+print("a <= b", (a <= b))
+print("a >= b", (a >= b))
+
+print("Aufgabe 2 logische Operationen")
+print("a >< b", (a > b) and (a < b))
+print("a <> b", (a < b) and (a > b))
+print("a >< b", (a > b) or (a < b))
+print("a >< b", (a > b) or (a < b))
+print
+      
+print("Aufgabe 3 Kombination von Operationen")
+print(a > b) # False
+print(a < b) # True
+
